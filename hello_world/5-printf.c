089ef8913f2c690a6ea05228b5063c2061bfdbda
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-18 17:10:08 +0000

    piece of art

---
+++ hello_world/5-printf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - printf function to print this is a piece of art 
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main() {
+    printf("This is a piece of art:\n");
+    printf("   *    *   \n");
+    printf("  * *  * *  \n");
+    printf(" *   **   * \n");
+    printf("  *       *  \n");
+    printf("   *     *   \n");
+    printf("    *   *    \n");
+    printf("     * *     \n");
+    printf("      *      \n");
+    return 0;
+}
