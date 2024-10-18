de8bf1f3fd7634b1c54bd014c2cdfa1cb3a7194f
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-18 17:17:16 +0000

    pieceofart

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - printf function to print this is a piece of art 
  *
  *  Return: Always 0 (Success)
  */
-
-int main() {
-    printf("This is a piece of art:\n");
-    printf("   *    *   \n");
-    printf("  * *  * *  \n");
-    printf(" *   **   * \n");
-    printf("  *       *  \n");
-    printf("   *     *   \n");
-    printf("    *   *    \n");
-    printf("     * *     \n");
-    printf("      *      \n");
-    return 0;
+int main()
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return 0;
 }
