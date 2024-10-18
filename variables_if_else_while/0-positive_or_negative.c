c188f325f4aeceaa3b4ba9968d42bdb53f48d9e3
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-18 19:43:25 +0000

    0

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - asasaradap 
+ *
+ *  Return: Always 0 (Success)
+ */
+int main()void)
+{
+	int main () {
+	srand(time(0));
+	int n = rand();
+	int last_digit = n % 10; 
+	n = rand() - RAND_MAX / 2;
+		return (0);
+}
