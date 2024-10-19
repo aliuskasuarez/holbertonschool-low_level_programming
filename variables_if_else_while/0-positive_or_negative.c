ace59da9b417d120cad7aa75e5f0d8526266d91a
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-19 17:50:39 +0000

    arreglos2

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - generates a random number and checks its last digit
  *
@@ -10,12 +9,10 @@
 int main(void) {
     int n;
     int last_digit;
-
     srand(time(0));
     n = rand();
     last_digit = n % 10;
     printf("Last digit of %d is %d\n", n, last_digit);
-
     return 0;
 }
 
