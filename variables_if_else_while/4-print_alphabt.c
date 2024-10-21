5b7ab12a42f38ea48f30768efe0f00bbd723f6a8
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-21 17:47:10 +0000

    creo que betty esta ok

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -4,14 +4,16 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
-    char letter;
-    for (letter = 'a'; letter <= 'z'; letter++) {
-        if (letter != 'q' && letter != 'e') {
-            putchar(letter);
-        }
-    }
-    putchar('\n');
-
-    return 0;
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if (letter != 'q' && letter != 'e')
+{
+putchar(letter);
+}
+}
+putchar('\n');
+return (0);
 }
