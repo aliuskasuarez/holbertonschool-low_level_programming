1fd7e0a68bcb8c68277b6472b819d69132e932c2
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-21 18:31:06 +0000

    print_numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (int i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+printf("\n");  
+return 0;
+}
