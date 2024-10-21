7fa1c4ec32cf9061031794a7e66a6038b5eabc8e
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-21 18:58:13 +0000

    numberz

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 /**
- * main - prints the alphabet in lowercase
+ * main - prints numbers from 0 to 10
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-for (int i = 0; i < 10; i++)
+int i;
+for (i = 0; i < 10; i++)
 {
 putchar(i + '0');
-}
+}    
 putchar('\n');
 return 0;
 }
