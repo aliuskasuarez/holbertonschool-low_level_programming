6dcaf4d81fc63fd9585d1391a59368bfa2672cd9
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-21 15:40:37 +0000

    1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+/**
+ * main  - dice si un numero random es positivo o negativo
+ *
+ * return: obvio siempre es 0 (Success)
+ */
 int main(void)
 {
     int n;
-    int last_digit;
     srand(time(0));
     n = rand() - RAND_MAX / 2;
-    last_digit = n % 10;
-    printf("Last digit of %d is: ", n);
-
-    if (last_digit > 5) {
-        printf("%d and is greater than 5\n", last_digit);
-    } else if (last_digit == 0) {
-        printf("%d and is 0\n", last_digit);
-    } else { 
-        printf("%d and is less than 6 and not 0\n", last_digit);
+    if (n % 10 > 5)
+    {
+    printf("last digit of %d is %d and is greater than 5\n", n, n % 10);
     }
+    else if (n % 10 == 0)
+    {
+	    printf("Last digit of %d is %d and i 0\n", n, n);
+    }
+    else if (n % 10 ! = 0 && n % 10 < 6)
+    {
+	    printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+    }
+
     return (0);
+
 }
