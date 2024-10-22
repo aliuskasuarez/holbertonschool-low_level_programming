29f21b568341487826c585bbf2eb2d99203b8185
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-22 15:57:57 +0000

    printcomb

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,23 +7,23 @@
 int putchar(int c);
 int main(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i <= 8; i++)
-    {
-        for (j = i + 1; j <= 9; j++)
-        {
-            putchar(i + '0');
+	for ( i = 0; i <= 8; i++)
+	{
+
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');
             putchar(j + '0');
-            
-            if (i != 8 || j != 9)
-            {
+
+	    if (i != 8 || j != 9)
+		    {
                 putchar(',');
                 putchar(' ');
             }
         }
     }
-
-    putchar('\n');
+	 putchar('\n');
     return (0);
 }
