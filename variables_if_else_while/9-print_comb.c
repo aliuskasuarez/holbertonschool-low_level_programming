ce943c47eef739887695f9206568559283db5bc2
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-22 19:25:32 +0000

    done

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -4,16 +4,17 @@
 *
 *Return 0 success
 */
-int main() {
-    int i;
-
-    for (i = 0; i <= 9; i++) {
-        putchar(i + '0');
-        if (i < 9) {
-            putchar(',');
-            putchar(' ');
-        }
-    }
-
-    return 0;
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+putchar(i + '0');
+if (i < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+return (0);
 }
