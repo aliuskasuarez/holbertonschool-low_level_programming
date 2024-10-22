66ec30d512d07ef4f2954b7a2e51dcc51b8d4c47
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-22 15:53:27 +0000

    printcomb

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,17 +1,21 @@
 #include <unistd.h>
+/*
+ * main print all possible combinations of single digit numbers separated by ,
+ *
+ * return always is 0 succes 
+ */
 int putchar(int c);
-
 int main(void)
 {
     int i, j;
 
-    for (i = 0; i <= 9; i++)
+    for (i = 0; i <= 8; i++)
     {
         for (j = i + 1; j <= 9; j++)
         {
             putchar(i + '0');
             putchar(j + '0');
-
+            
             if (i != 8 || j != 9)
             {
                 putchar(',');
