10ceb1d34ba1a283599370e76e51c0e7dcbb16a2
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-22 15:30:45 +0000

    8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -2,10 +2,14 @@
 /**
  * main - prints hexagonal numbers of base 16
  *
- * putchar - writes a character to the standard output
  * Return: Always 0 (Success)
  */
 int putchar(int c);
+/**
+ * main - prints hexadecimal numbers of base 16
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 char hex[] = "0123456789abcdef";
