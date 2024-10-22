8c2280c810794aed9760bd556b9a78af087aaca9
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-22 19:20:46 +0000

    done

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,4 +1,3 @@
-#include <unistd.h>
 #include <stdio.h>
 /**
 * main i will be using putchar to print all posible combinations
@@ -9,13 +8,12 @@ int main() {
     int i;
 
     for (i = 0; i <= 9; i++) {
-      
-            putchar(i + '0');
-            if (i < 9 {
-                putchar(',');
-                putchar(' ');
-          }
-        
-   }
-return (0);
+        putchar(i + '0');
+        if (i < 9) {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+
+    return 0;
 }
