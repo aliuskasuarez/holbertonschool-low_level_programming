9eb6dc5e1f18738efbcc18f249045b66a6f11f64
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-22 18:33:17 +0000

    inclui stdio.h y borre el comentario de la coma

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,20 +1,24 @@
 #include <unistd.h>
+#include <stdio.h>
 /*
  * main i will be using putchar to print all posible combinations
  *
  * return 0 success 
  */ 
 int main() 
-{int i, j; 
+{
+	int i, j; 
+
 	for (i = 0; i<= 9; i++) 
 	{
-		for (j = i + 1; j <= 9; j++)
+		for (j = i + 1; j<= 9; j++) 
 		{
-	putchar(i + '0');
 	putchar(j + '0');
+	putchar(j + '0');
+
 	if (i < 8 || j < 9) 
 	{
-		putchar(','); //esto imprime la coma  mau 
+		putchar(',');
                 putchar(' '); 
 		}
 		}
