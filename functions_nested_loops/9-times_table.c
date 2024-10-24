43950e20ab29ef8a1a489edee730dbe918f26f41
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-24 03:09:18 +0000

    ejercicio9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,17 +1,25 @@
 #include "main.h"
-void times_table(void) {
-    int i, j;
-    char buffer[50];
+#include <stdio.h>
 
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
-            int product = i * j;
-            int len = snprintf(buffer, sizeof(buffer), "%2d", product);
-            write(1, buffer, len);
-            if (j < 9) {
-                write(1, ", ", 2);
-            }
-        }
-        write(1, "$\n", 2);
-    }
+/**
+ * times_table - imprime la tabla del 9 empezando de 0
+ *
+ * imprime la tabla del 9
+ */
+void times_table(void)
+{
+int i, product;
+for (i = 0; i <= 9; i++)
+{
+product = 9 * i;
+if (i == 9)
+{
+printf("%d", product);
+}
+else
+{
+printf("%d, ", product);
+}
+}
+printf("\n");
 }
