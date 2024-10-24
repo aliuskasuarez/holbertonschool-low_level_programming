715f523114fb2bfe1af271d63717529e61620d3f
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-24 16:33:28 +0000

    102bettyok

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -3,28 +3,33 @@
 /**
  * main - printea los 50 fibo
  *
- * Description: genera fibonacci 50 primeros 
+ * Description:genera fibonacci 50 primeros
  *
  * Return: siempre exitoso 0
  */
-int main(void) {
-    unsigned long fib1 = 1, fib2 = 2;
-    unsigned long next_fib;
-    int count;
-
-    for (count = 1; count <= 50; count++) {
-        if (count == 1) {
-            printf("%lu", fib1);
-        } else if (count == 2) {
-            printf(", %lu", fib2);
-        } else {
-            next_fib = fib1 + fib2;
-            printf(", %lu", next_fib);
-            fib1 = fib2;
-            fib2 = next_fib;
-        }
-    }
-
-    printf("\n");
-    return 0;
+int main(void)
+{
+unsigned long fib1 = 1, fib2 = 2;
+unsigned long next_fib;
+int count;
+for (count = 1; count <= 50; count++)
+{
+if (count == 1)
+{
+printf("%lu", fib1);
+}
+else if (count == 2)
+{
+printf(", %lu", fib2);
+}
+else
+{
+next_fib = fib1 + fib2;
+printf(", %lu", next_fib);
+fib1 = fib2;
+fib2 = next_fib;
+}
+}
+printf("\n");
+return (0);
 }
