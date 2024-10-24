4e76becf48c30416c13db6244083bd8d1c249867
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-24 17:54:17 +0000

    prueba104

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -1,38 +1,33 @@
 #include <stdio.h>
-void _putchar(char c);
-void _putchar(char c)
-{
-putchar(c);
+
+void _putchar(char c) {
+    putchar(c);
 }
-void print_comb3(void)
-{
-int i, j, k;
-for (i = 0; i < 8; i++)
-{
-for (j = i + 1; j < 9; j++)
-{
-for (k = j + 1; k < 10; k++)
-{
-if (i == 0 && j == 1 && k == 2)
-{
-_putchar('0' + i);
-_putchar('0' + j);
-_putchar('0' + k);
+
+void print_comb3(void) {
+    int i, j, k;
+
+    for (i = 0; i < 8; i++) {
+        for (j = i + 1; j < 9; j++) {
+            for (k = j + 1; k < 10; k++) {
+                if (i == 0 && j == 1 && k == 2) {
+                    _putchar('0' + i);
+                    _putchar('0' + j);
+                    _putchar('0' + k);
+                } else {
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar('0' + i);
+                    _putchar('0' + j);
+                    _putchar('0' + k);
+                }
+            }
+        }
+    }
+    _putchar('\n');
 }
-else
-{
-_putchar(',');
-_putchar(' ');
-_putchar('0' + i);
-_putchar('0' + j);
-_putchar('0' + k);
-}
-}
-}
-_putchar('\n');
-}
-int main(void)
-{
-print_comb3();
-return 0;
+
+int main(void) {
+    print_comb3();
+    return 0;
 }
