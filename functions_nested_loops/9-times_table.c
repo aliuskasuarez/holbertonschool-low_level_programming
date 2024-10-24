e9abde9f678c9eee2cc62a19138dbd2f61647e73
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-24 11:35:09 +0000

    9-times_table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,13 +8,15 @@ for (i = 0; i < 10; i++)
 for (j = 0; j < 10; j++)
 {
 product = i * j;
-if (j == 0) {
+if (j == 0)
+{
 _putchar(product + '0');
 }
 else
 {
 _putchar(',');
-if (product < 10) {
+if (product < 10)
+{
 _putchar(' '); 
 _putchar(' ');
 _putchar(product + '0');
