30072c47f0d0391983f3ca92afe64a456305216a
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-24 00:10:05 +0000

    devuelve el gcc

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,6 +1,11 @@
 #include "main.h"
-
+/**
+ * print_alphabet_x10 - alphabet in lowercase 10 times
+ *
+ * Description:This function prints 10 times the alphabet in lowecase
+ *
+ */
 int _islower(int c)
 {
-    return (c >= 'a' && c <= 'z') ? 1 : 0;
+return (c >= 'a' && c <= 'z') ? 1 : (0);
 }
