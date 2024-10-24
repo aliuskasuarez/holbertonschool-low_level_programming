98027569a3557fdc8f0c6f93b1f567e9dcf1a9c3
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-24 02:32:38 +0000

    24hours

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,17 +1,20 @@
-#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
 void jack_bauer(void)
 {
 int hour, minute;
+char time_str[6];
 for (hour = 0; hour < 24; hour++)
 {
 for (minute = 0; minute < 60; minute++)
 {
-putchar('0' + (hour / 10));
-putchar('0' + (hour % 10));
-putchar(':');
-putchar('0' + (minute / 10));
-putchar('0' + (minute % 10));
-putchar('\n');
+time_str[0] = '0' + (hour / 10);
+time_str[1] = '0' + (hour % 10);
+time_str[2] = ':';
+time_str[3] = '0' + (minute / 10);
+time_str[4] = '0' + (minute % 10);
+time_str[5] = '\n';
+write(1, time_str, 6);
 }
 }
 }
