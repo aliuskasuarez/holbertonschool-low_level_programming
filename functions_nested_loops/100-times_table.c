4c990160c065daa1b28493b26f089df35bd4e448
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-24 16:15:32 +0000

    101

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,35 +1,24 @@
-void print_times_table(int n) {
-    if (n < 0 || n > 15) {
-        return;
-    }
+#include "main.h"
+#include <stdio.h>
 
+void custom_print(int num) {
+    printf("%d", num);
+}
+
+void print_times_table(int n) {
     int i, j, product;
 
     for (i = 0; i <= n; i++) {
         for (j = 0; j <= n; j++) {
             product = i * j;
-
-            // Convert product to string and print it
             char buffer[4];
-            int index = 0;
-
-            if (product == 0) {
-                buffer[index++] = '0';
-            } else {
-                int num = product;
-                while (num > 0) {
-                    buffer[index++] = (num % 10) + '0';
-                    num /= 10;
-                }
-            }
-
-            // Reverse the buffer to get the correct order
-            for (int k = index - 1; k >= 0; k--) {
-                custom_print(buffer[k]);
+            sprintf(buffer, "%d", product);
+            int k;
+            for (k = 0; buffer[k] != '\0'; k++) {
+                custom_print(buffer[k] - '0');
             }
 
             if (j < n) {
-                custom_print(',');
                 custom_print(' ');
             }
         }
---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+int main()
+{
+int limit = 1024;
+int sum = 0;
+for (int i = 0; i < limit; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+sum += i;
+}
+}
+printf("%d\n", sum);
+return (0);
+}
