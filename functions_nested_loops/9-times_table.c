b82d10488a6bac78a649c57490dd09da9ef280fc
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-24 10:39:19 +0000

    tabladel9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,18 +1,31 @@
 #include <unistd.h>
 #include "main.h"
-void print_times_table(int n) {
-    int i, result;
-
-    for (i = 0; i <= n; i++) {
-        result = i * 9;
-        if (result < 10) {
-            _putchar(result + '0');
-        } else {
-            _putchar((result / 10) + '0');
-            _putchar((result % 10) + '0');
-        }
-        if (i < n) {
-            _putchar('\n');
-        }
-    }
+/**
+ * times_table - imprime la tabla del 9
+ *
+ * esta funcion imprime la tabla del 9 el codigo es echo por mi mauricio
+ *
+ * Return: void
+ */
+int _putchar(char c);
+void times_table(void)
+{
+int i, result;
+for (i = 0; i <= 9; i++)
+{
+result = i * 9;
+if (result < 10)
+{
+_putchar(result + '0');
+}
+else
+{
+_putchar((result / 10) + '0');
+_putchar((result % 10) + '0');
+}
+if (i < 9)
+{
+_putchar('\n');
+}
+}
 }
