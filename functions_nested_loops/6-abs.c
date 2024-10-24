67b0e27e01280b4109a8dfb011fc476418c54df0
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-24 01:35:28 +0000

    6-abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - imprime el valor absoluto
+ * @n: el integrador
+ *
+ * Return: el valor absoluto de n
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+return (n);
+}
