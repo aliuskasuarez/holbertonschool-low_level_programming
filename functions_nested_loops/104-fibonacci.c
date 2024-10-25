96655dc43f432cd7905c7dc2310672f275117316
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-25 18:21:02 +0000

    104

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -2,24 +2,20 @@
 
 int main(void)
 {
-    unsigned long fib1 = 1, fib2 = 2;
-    unsigned long next_fib;
-    unsigned long sum = fib1 + fib2;
+    unsigned long long fib1 = 1, fib2 = 2, next_fib;
     int i;
 
-    printf("%lu, %lu", fib1, fib2);
+    printf("%llu, %llu", fib1, fib2);
 
     for (i = 3; i <= 98; i++)
     {
         next_fib = fib1 + fib2;
-        printf(", %lu", next_fib);
-        sum += next_fib;
-
+        printf(", %llu", next_fib);
         fib1 = fib2;
         fib2 = next_fib;
     }
 
-    printf(", %lu\n", sum);
+    printf("\n");
     return 0;
 }
 
