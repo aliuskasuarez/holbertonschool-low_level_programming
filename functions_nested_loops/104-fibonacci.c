ed839296740c764513d49c0d8bb779a98ea56397
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-25 18:22:49 +0000

    104

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,21 +1,45 @@
 #include <stdio.h>
 
-int main(void)
-{
-    unsigned long long fib1 = 1, fib2 = 2, next_fib;
-    int i;
+#define MAX_DIGITS 100
 
-    printf("%llu, %llu", fib1, fib2);
+void add_fibonacci(char result[][MAX_DIGITS], int n) {
+    int carry, i;
 
-    for (i = 3; i <= 98; i++)
-    {
-        next_fib = fib1 + fib2;
-        printf(", %llu", next_fib);
-        fib1 = fib2;
-        fib2 = next_fib;
+    result[0][0] = '1';
+    result[0][1] = '\0';
+    result[1][0] = '2';
+    result[1][1] = '\0';
+
+    for (int j = 2; j < n; j++) {
+        carry = 0;
+        for (i = 0; result[j - 1][i] != '\0' || result[j - 2][i] != '\0' || carry; i++) {
+            int sum = carry;
+            if (result[j - 1][i] != '\0') {
+                sum += result[j - 1][i] - '0';
+            }
+            if (result[j - 2][i] != '\0') {
+                sum += result[j - 2][i] - '0';
+            }
+            result[j][i] = (sum % 10) + '0';
+            carry = sum / 10;
+        }
+        result[j][i] = '\0';
     }
-
-    printf("\n");
-    return 0;
 }
 
+void print_fibonacci(char result[][MAX_DIGITS], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%s", result[i]);
+        if (i < n - 1) {
+            printf(", ");
+        }
+    }
+    printf("\n");
+}
+
+int main(void) {
+    char fib[98][MAX_DIGITS];
+    add_fibonacci(fib, 98);
+    print_fibonacci(fib, 98);
+    return 0;
+}
