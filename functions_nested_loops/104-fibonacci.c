3c6ae9d477ab845510bcfa0caf552bd7a3b53dcf
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-25 16:50:20 +0000

    104fibo

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,33 +1,41 @@
 #include <stdio.h>
 
 /**
- * main - imprime fiboooo 
+ * main - Prints the first 98 Fibonacci numbers and their sum
  *
- * Description: este programa genera hasta 400000 sin exeder eso el fiboo
+ * Description: This program generates the first 98 Fibonacci numbers
+ *              and calculates the sum of the even ones.
  *
- * Return: siempre retorna 0 el exitoso
+ * Return: Always returns 0 on success
  */
 int main(void)
 {
-unsigned long fib1 = 1, fib2 = 2;
-unsigned long next_fib;
-unsigned long sum = 0;
-printf("%lu, %lu", fib1, fib2);
-sum += fib2;
-while (fib2 <= 4000000)
-{
-next_fib = fib1 + fib2;
-if (next_fib > 4000000)
-{
-break;
-}
-printf(", %lu", next_fib);
-if (next_fib % 2 == 0) {
-sum += next_fib;
-}
-fib1 = fib2;
-fib2 = next_fib;
-}
-printf("\nSum of even Fibonacci numbers: %lu\n", sum);
-return (0);
+    unsigned long fib1 = 1, fib2 = 2;
+    unsigned long next_fib;
+    unsigned long sum = 0;
+
+    printf("%lu", fib1);
+    sum += fib1;
+
+    for (int i = 1; i < 98; i++)
+    {
+        if (i == 1) {
+            printf(", %lu", fib2);
+            sum += fib2;
+            continue;
+        }
+
+        next_fib = fib1 + fib2;
+        printf(", %lu", next_fib);
+
+        if (next_fib % 2 == 0) {
+            sum += next_fib;
+        }
+
+        fib1 = fib2;
+        fib2 = next_fib;
+    }
+
+    printf("\nSum of even Fibonacci numbers: %lu\n", sum);
+    return (0);
 }
