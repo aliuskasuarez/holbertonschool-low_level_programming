f6be7b18c1dc7a8a72ed2cde2e40cb9fe15a9e76
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-25 17:40:03 +0000

    104fibo

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+
 int main(void)
 {
-    unsigned long long fib1 = 1, fib2 = 2;
-    unsigned long long next_fib;
-    unsigned long long sum = fib1 + fib2;
+    unsigned long fib1 = 1, fib2 = 2;
+    unsigned long next_fib;
+    unsigned long sum = fib1 + fib2;
     int i;
 
-    printf("%llu, %llu", fib1, fib2);
+    printf("%lu", fib1);
+    printf(", %lu", fib2);
 
     for (i = 2; i < 98; i++)
     {
         next_fib = fib1 + fib2;
-        printf(", %llu", next_fib);
+        printf(", %lu", next_fib);
 
         sum += next_fib;
 
@@ -19,6 +21,6 @@ int main(void)
         fib2 = next_fib;
     }
 
-    printf("\nsuma de los fibonacci numbers: %llu\n", sum);
-    return (0);
+    printf("\nsuma de los fibonacci numbers: %lu\n", sum);
+    return 0;
 }
