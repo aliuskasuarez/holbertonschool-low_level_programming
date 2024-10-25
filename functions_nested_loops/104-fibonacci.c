b3b4c6aad51239093d347a5566f928e534d9b6fb
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-25 18:24:51 +0000

    104

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -3,15 +3,16 @@
 #define MAX_DIGITS 100
 
 void add_fibonacci(char result[][MAX_DIGITS], int n) {
-    int carry, i;
+    int carry, i, j;
 
     result[0][0] = '1';
     result[0][1] = '\0';
     result[1][0] = '2';
     result[1][1] = '\0';
 
-    for (int j = 2; j < n; j++) {
+    for (j = 2; j < n; j++) {
         carry = 0;
+
         for (i = 0; result[j - 1][i] != '\0' || result[j - 2][i] != '\0' || carry; i++) {
             int sum = carry;
             if (result[j - 1][i] != '\0') {
@@ -28,7 +29,9 @@ void add_fibonacci(char result[][MAX_DIGITS], int n) {
 }
 
 void print_fibonacci(char result[][MAX_DIGITS], int n) {
-    for (int i = 0; i < n; i++) {
+    int i;
+
+    for (i = 0; i < n; i++) {
         printf("%s", result[i]);
         if (i < n - 1) {
             printf(", ");
