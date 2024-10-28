08df857dfeccee1f3973ad6ef0bd381318828631
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-28 20:58:58 +0000

    milaberinto

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * mul - Multiplica 2 integradores
+ * @a: el primer integrador
+ * @b: el segundo inegrador
+ *
+ * Return: el prod de 2 integradores
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
