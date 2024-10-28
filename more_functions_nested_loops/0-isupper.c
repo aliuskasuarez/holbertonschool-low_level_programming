4b31dabb6384088c3db2d852de042a69dcc52f12
aliuskasuarez <aliuskasuarez87@gmail.com>
2024-10-28 19:47:02 +0000

    ok

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - checkea mayuscula
+ * @c:
+ *
+ * Return: 1 mayuscula de otra manera 0
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
